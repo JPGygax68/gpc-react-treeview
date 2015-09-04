dd2d0fb22b44f9dd89f9c1dd3691e096026a35ed
Jean-Pierre Gygax <gygax@practicomp.ch>
2015-09-04 20:13:40 +0200

    Starting to see the pitfalls of not using JSX

--- src/main.js
+++ src/main.js
@@ -4,10 +4,23 @@
 var React = require('react');
 //require('react/dist/JSXTransformer');
 
+var Comment = React.createClass({
+  displayName: 'Comment',
+  render: function() {
+    return React.createElement('div', {className: 'comment'},
+      React.createElement('h2', {className: 'commentAuthor'}, this.props.author),
+      this.props.children      
+    );
+  }
+});
+
 var CommentList = React.createClass({
   displayName: 'CommentList',
   render: function() {
-    return React.createElement('div', {className: 'commentList'}, 'Hello, World! I\'m a comment list');
+    return React.createElement('div', {className: 'commentList'}, 
+      React.createElement(Comment, {author: 'Pete Hunt'}, 'This is one comment'),
+      React.createElement(Comment, {author: 'Jordan Walke'}, 'This is *another* comment')
+    );
   }
 });
 
@@ -24,8 +37,8 @@ var CommentBox = React.createClass({
     console.log('here');
     return React.createElement('div', null,
       React.createElement('h1', null, 'Comments'),
-      React.createElement('CommentList', null),
-      React.createElement('CommentForm', null) 
+      React.createElement(CommentList, null),
+      React.createElement(CommentForm, null) 
     );
   }
 });
