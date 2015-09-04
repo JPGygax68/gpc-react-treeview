93aeeabd83bbe5c37d6128f69cfeacc4846f2a8f
Jean-Pierre Gygax <gygax@practicomp.ch>
2015-09-04 19:57:12 +0200

    Doing the tutorial but without using JSX

--- src/main.js
+++ src/main.js
@@ -1,9 +1,34 @@
 "use strict";
 
 //var insertCss = require('insert-css'); // JUST AN EXAMPLE
-var react = require('react');
-require('react/dist/JSXTransformer');
-console.log('react:', react);
+var React = require('react');
+//require('react/dist/JSXTransformer');
+
+var CommentList = React.createClass({
+  displayName: 'CommentList',
+  render: function() {
+    return React.createElement('div', {className: 'commentList'}, 'Hello, World! I\'m a comment list');
+  }
+});
+
+var CommentForm = React.createClass({
+  displayName: 'CommentForm',
+  render: function() {
+    return React.createElement('div', {className: 'commentForm'}, 'Hello, World! I\'m a comment form');
+  }
+});
+
+var CommentBox = React.createClass({
+  displayName: 'CommentBox',
+  render: function() {
+    console.log('here');
+    return React.createElement('div', null,
+      React.createElement('h1', null, 'Comments'),
+      React.createElement('CommentList', null),
+      React.createElement('CommentForm', null) 
+    );
+  }
+});
 
 module.exports = {
   
@@ -11,7 +36,11 @@ module.exports = {
     //insertCss( require("./org-unnamed.styl") );
   },
 
-  react: react
+  CommentBox: CommentBox,
+  //CommentList: CommentList,
+  //CommentForm: CommentForm,
+  
+  React: React
   
   // Examples of convenience exports
   //ko: ko,
